bd6f1906e7cee0e65b42dcf8c3d4c8fdacb98b4a
Ja <test@mail.com>
2018-03-12 09:13:20 +0100

    Delta ujemna

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -10,13 +10,22 @@ namespace RownaniaKwadratowe
     {
         static void Main(string[] args)
         {
-            double a, b, c;
+            double a, b, c, x1, x2;
             a = 1;
             b = 2;
             c = -10;
+            x1 = 0;x2 = 0;
             double delta = b * b - 4 * a * c;
-            double x1 = (-b - Math.Sqrt(delta))/ 2 * a;
-            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+            if (delta >= 0)
+            {
+                x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+            }
+            else
+            {
+                Console.WriteLine("Brak rozwiązań rzeczywistych");
+            }
+            
             Console.WriteLine("Pierwiastki to: {0} i {1}", x1, x2);
         }
     }
